d47ec8b6222bd5eb5d920c1179dadcf9a0ba4981
Amirul Ikmal <amirulikmal.biz@gmail.com>
2023-05-29 11:26:08 +0800

    reduce margin on hero

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -16,7 +16,7 @@ export const Hero: FC = () => {
       <motion.img
         src="/images/me.jpg"
         alt="A beautiful face"
-        className="overflow-hidden rounded-full w-28 h-28 md:w-32 md:h-32 mt-36"
+        className="overflow-hidden rounded-full w-14 h-14 md:w-16 md:h-16 mt-18"
         variants={{
           hidden: {
             scale: 0,
